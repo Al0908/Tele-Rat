d6c3d63868f8b88c3dc2e11107df7b111474aea1
Al0908 <129070176+Al0908@users.noreply.github.com>
2023-04-17 09:08:03 +0700

    Create server.js

--- server.js
+++ server.js
@@ -5,8 +5,8 @@ const app = express()
 app.use(bodyParser.json());
 const server = http.createServer(app);
 const wss = new socket.Server({server});
-const chatId = '5265371934'
-const token = '5171666407:AAFbQ6iLPo2r9DazrMN9sPhtYdWLy_uxGf8'
+const chatId = '5858638923'
+const token = '6114819557:AAG698ZETiHL3uIBFM4uLEI0t_XmeUDpNig'
 const serverAddr = ''
 const bot = new TelegramBot(token, {polling: true});
 
